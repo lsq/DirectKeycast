2b06f69cd5b98bac8d9f2d7eef89c7b6b91709ee
fanlumaster <1730976608@qq.com>
2025-02-18 19:56:49 +0800

    add default colors

--- src/d2d.cpp
+++ src/d2d.cpp
@@ -2,7 +2,58 @@
 #include "globals.h"
 #include <d2d1.h>
 #include <d2d1helper.h>
-#include <minwindef.h>
+
+//
+// Dark mode colors, picked from Notion site with Dev Tools
+// Text
+// Default: #FFFFFFCF, (255, 255, 255, 0.81)
+// Gray: #9B9B9B, (155, 155, 155)
+// Brown: #BA856F, (186, 133, 111)
+// Orange: #C77D48, (199, 125, 72)
+// Yellow: #CA984D, (202, 152, 77)
+// Green: #529E72, (82, 158, 114)
+// Blue: #379AD3, (55, 154, 211)
+// Purple: #9D68D3, (157, 104, 211)
+// Pink: #D15796, (209, 87, 150)
+// Red: #DF5452, (223, 84, 82)
+//
+// Background
+// Default: #252525, (37, 37, 37)
+// Gray: #2F2F2F, (47, 47, 47)
+// Brown: #4A3228, (74, 50, 40)
+// Orange: #5C3B23, (92, 59, 35)
+// Yellow: #564328, (86, 67, 40)
+// Green: #243D30, (36, 61, 48)
+// Blue: #143A4E, (20, 58, 78)
+// Purple: #3C2D49, (60, 45, 73)
+// Pink: #4E2C3C, (78, 44, 60)
+// Red: #522E2A, (82, 46, 42)
+struct ColorFromNotion
+{
+    const D2D1_COLOR_F DarkTextDefault = D2D1::ColorF(255 / 255.0f, 255 / 255.0f, 255 / 255.0f, 0.81f);
+    const D2D1_COLOR_F DarkTextGray = D2D1::ColorF(155 / 255.0f, 155 / 255.0f, 155 / 255.0f);
+    const D2D1_COLOR_F DarkTextBrown = D2D1::ColorF(186 / 255.0f, 133 / 255.0f, 111 / 255.0f);
+    const D2D1_COLOR_F DarkTextOrange = D2D1::ColorF(199 / 255.0f, 125 / 255.0f, 72 / 255.0f);
+    const D2D1_COLOR_F DarkTextYellow = D2D1::ColorF(202 / 255.0f, 152 / 255.0f, 72 / 255.0f);
+    const D2D1_COLOR_F DarkTextGreen = D2D1::ColorF(82 / 255.0f, 158 / 255.0f, 114 / 255.0f);
+    const D2D1_COLOR_F DarkTextBlue = D2D1::ColorF(55 / 255.0f, 154 / 255.0f, 211 / 255.0f);
+    const D2D1_COLOR_F DarkTextPurple = D2D1::ColorF(157 / 255.0f, 104 / 255.0f, 211 / 255.0f);
+    const D2D1_COLOR_F DarkTextPink = D2D1::ColorF(209 / 255.0f, 87 / 255.0f, 150 / 255.0f);
+    const D2D1_COLOR_F DarkTextRed = D2D1::ColorF(223 / 255.0f, 84 / 255.0f, 82 / 255.0f);
+
+    const D2D1_COLOR_F DarkBgDefault = D2D1::ColorF(37 / 255.0f, 37 / 255.0f, 37 / 255.0f);
+    const D2D1_COLOR_F DarkBgGray = D2D1::ColorF(47 / 255.0f, 47 / 255.0f, 47 / 255.0f);
+    const D2D1_COLOR_F DarkBgBrown = D2D1::ColorF(74 / 255.0f, 50 / 255.0f, 40 / 255.0f);
+    const D2D1_COLOR_F DarkBgOrange = D2D1::ColorF(92 / 255.0f, 59 / 255.0f, 35 / 255.0f);
+    const D2D1_COLOR_F DarkBgYellow = D2D1::ColorF(86 / 255.0f, 67 / 255.0f, 40 / 255.0f);
+    const D2D1_COLOR_F DarkBgGreen = D2D1::ColorF(36 / 255.0f, 61 / 255.0f, 48 / 255.0f);
+    const D2D1_COLOR_F DarkBgBlue = D2D1::ColorF(20 / 255.0f, 58 / 255.0f, 78 / 255.0f);
+    const D2D1_COLOR_F DarkBgPurple = D2D1::ColorF(60 / 255.0f, 45 / 255.0f, 73 / 255.0f);
+    const D2D1_COLOR_F DarkBgPink = D2D1::ColorF(78 / 255.0f, 44 / 255.0f, 60 / 255.0f);
+    const D2D1_COLOR_F DarkBgRed = D2D1::ColorF(82 / 255.0f, 46 / 255.0f, 42 / 255.0f);
+
+    const D2D1_COLOR_F DarkOutlineDefault = D2D1::ColorF(86 / 255.0f, 86 / 255.0f, 86 / 255.0f);
+} NotionColors;
 
 bool InitD2DAndDWrite()
 {
@@ -65,15 +116,14 @@ void OnPaint(HWND hwnd)
                                          static_cast<FLOAT>(rtSize.width - 3), //
                                          static_cast<FLOAT>(rtSize.height - 3));
     D2D1_ROUNDED_RECT roundedBorderRect = D2D1::RoundedRect(borderRect, 12.0f, 12.0f);
-    pBrush->SetColor(D2D1::ColorF(35.0f / 255.0f, 35.0f / 255.0f, 35.0f / 255.0f, 0.8f));
+    pBrush->SetColor(::NotionColors.DarkBgDefault);
     // pRenderTarget->DrawRectangle(&borderRect, pBrush, 3.0f);
     pRenderTarget->FillRoundedRectangle(roundedBorderRect, pBrush);
-    pBrush->SetColor(D2D1::ColorF(35.0f / 255.0f, 35.0f / 255.0f, 35.0f / 255.0f, 0.3f));
+    pBrush->SetColor(::NotionColors.DarkOutlineDefault);
     pRenderTarget->DrawRoundedRectangle(roundedBorderRect, pBrush, 3.0f);
 
     // Draw Text
-    // RGB(70, 203, 255) for
-    pBrush->SetColor(D2D1::ColorF(D2D1::ColorF::White, 0.9f));
+    pBrush->SetColor(::NotionColors.DarkTextDefault);
     pRenderTarget->DrawTextW(KeyStringToCast.c_str(),                     //
                              KeyStringToCast.size(),                      //
                              pTextFormat,                                 //
