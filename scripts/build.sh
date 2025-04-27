068f2bd8d8b3b309760f56816aba57eeffacbb3a
lsq <lsqypj.gmail.com>
2025-04-27 09:58:26 +0800

    rename release name format for scoop update

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,5 @@
 
 cmake -DCMAKE_CXX_COMPILER=g++ -DCMAKE_CC_COMPILER=gcc -DCMAKE_MAKE_PROGRAM=mingw32-make -G "MinGW Makefiles"  -S . -B ./build/
 cmake --build ./build/ 
-[ -f ./build/bin/directkeycast.exe ] && cp ./build/bin/directkeycast.exe ./directkeycast.exe
+commitId=$(git log -1 --format=%h)
+[ -f ./build/bin/directkeycast.exe ] && cp ./build/bin/directkeycast.exe ./directkeycast-r"${commitId}".exe
