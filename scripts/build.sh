f99ae37807b5dc51b651e7756644f981c23e8019
fanlumaster <1730976608@qq.com>
2025-06-18 21:53:56 +0800

    No need for github actions

--- scripts/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-cmake -DCMAKE_CXX_COMPILER=g++ -DCMAKE_CC_COMPILER=gcc -DCMAKE_MAKE_PROGRAM=mingw32-make -G "MinGW Makefiles"  -S . -B ./build/
-cmake --build ./build/ 
-[ -f ./build/bin/directkeycast.exe ] && cp ./build/bin/directkeycast.exe ./directkeycast.exe
